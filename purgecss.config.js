343885b074c7b79511371d88035750116e4110db
EwenLeFloch <ewen.lefloch96@gmail.com>
2023-07-26 15:09:40 +0200

    bootstrap local + purge, json-ld pour referencement local

---
+++ purgecss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: ["index.html", "assets/maugallery.js", "assets/scripts.js"],
+	css: [
+		"assets/bootstrap/bootstrap.css",
+		"assets/bootstrap/bootstrap.min.css",
+	],
+	output: "assets/bootstrap/",
+	fontFace: true,
+	keyframes: true,
+	variables: true,
+	safelist: ["modal", /^col-/],
+};
